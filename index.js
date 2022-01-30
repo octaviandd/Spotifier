53e77c21ab15b458258907f1623fc8f91b7c90a9
Octavian David <45077244+octaviandd@users.noreply.github.com>
2022-01-30 18:29:43 +0000

    redirect uri

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var client_secret = process.env.CLIENT_SECRET;
 
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
